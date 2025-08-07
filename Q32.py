a0253b9c2565fb62467aadde6765fdf2cca17d1b
Muhammad Humayun <humayunmuhammad902@gmail.com>
2025-08-07 13:03:34 +0500

    Converting miles to kilometer.

---
+++ Q32.py
@@ -0,0 +1,14 @@
+
+
+    # Convert miles to kilometers
+
+
+def convert(miles):
+    km = 1.6 * miles
+    print("Converting miles to kilometers: ")
+    return km
+
+
+result = float(input("Enter miles: "))
+
+print("The result is: ", convert(result), "km")
